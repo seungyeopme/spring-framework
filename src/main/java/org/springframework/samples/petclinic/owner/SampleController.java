db79373569f125815e6648d101abf2241ac83413
seungyeopme <seungyeop.me.dev@gmail.com>
2021-08-05 21:50:23 +0900

    IoC(Inversion of Control)컨테이너

---
+++ src/main/java/org/springframework/samples/petclinic/owner/SampleController.java
@@ -0,0 +1,19 @@
+package org.springframework.samples.petclinic.owner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@GetMapping("/context")
+	public String context() {
+		return "hello" + applicationContext.getBean(OwnerRepository.class);
+	}
+
+}
